69e2c927d186c33546df9b1661f5c8a23846ef86
achmadsatyap <satyapradana98@gmail.com>
2018-11-29 21:30:43 +0700

    membuat class transaksi

---
+++ src/com/satya/transaksi/Transaksi.java
@@ -0,0 +1,16 @@
+package com.satya.transaksi;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author satya
+ */
+import java.util.*;
+public class Transaksi {
+
+}
